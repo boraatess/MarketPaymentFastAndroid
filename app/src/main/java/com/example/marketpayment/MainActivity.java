f2cf64e9bb86f9e91d4813e442c167ae67ef5a31
boraates <boraatess@outlook.com>
2020-09-23 05:49:06 +0300

    keep development

--- app/src/main/java/com/example/marketpayment/MainActivity.java
+++ app/src/main/java/com/example/marketpayment/MainActivity.java
@@ -5,14 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aSwitch = findViewById(R.id.switch2);
+
+
+    }
+
+    public void ForgotPassword(View view)
+    {
+        aSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+             Intent intent = new Intent(MainActivity.this,ForgotPassword.class);
+             startActivity(intent);
+
+            }
+        });
     }
 
     public void onDefaultToggleClick(View view)
--- app/src/main/java/com/example/marketpayment/MainPageActivity.java
+++ app/src/main/java/com/example/marketpayment/MainPageActivity.java
@@ -1,14 +1,89 @@
 package com.example.marketpayment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Switch;
 
-public class MainPageActivity extends AppCompatActivity {
+public class MainPageActivity extends AppCompatActivity
+{
+    Switch aSwitch;
+    EditText editText ;
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        aSwitch = findViewById(R.id.switch3);
+        editText = findViewById(R.id.PersonName3);
+        imageButton = findViewById(R.id.imageButton3);
+
+        editText.setEnabled(false);
+
+        aSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (aSwitch.isChecked())
+                {
+                 editText.setEnabled(true);
+                }
+                else
+                {
+                    editText.setEnabled(false);
+                }
+            }
+        });
+
     }
+
+    public void TakePhoto(View view)
+    {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            imageButton.setEnabled(true);
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA,
+
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 0);
+
+        }
+
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Resim çekme isteği ve activity başlatılıp id'si tanımlandı
+        startActivityForResult(camera,33);
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 33) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            //Çekilen resim id olarak bitmap şeklinde alındı ve imageview'e atandı
+            ImageView resim= (ImageView)findViewById(R.id.imageView); resim.setImageBitmap(image);
+        }
+    }
+
+
+
+    public void ReadBarcode(View view)
+    {
+        //TakePhoto(view);
+
+    }
+
 }
